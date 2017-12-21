e4ce2136779290abdfd6853db6873f8c92a6b4e3
JC <jc@jollant.net>
2017-12-21 19:57:51 +0100

    Fixed disconnection

--- status.js
+++ status.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env nodejs
+#!/usr/bin/node
 //const express = require('express')
 const app = require('express')()
 const mysql = require('mysql')
@@ -6,32 +6,37 @@ const port = 3000
 
 app.enable('trust proxy')
 
+var cnx
+
 function dbConnect() {
-	return mysql.createConnection({
+	cnx = mysql.createConnection({
 	  host: 'localhost',
 	  user: 'root',
 	  password: 'rootpw',
 	  database: 'status'
 	});
+	cnx.connect( (err) => {
+		if( err) throw err;
+		console.log( 'DB Connection up');
+		// keep the connection alive
+		setInterval( () => {
+			cnx.query('SELECT 1')
+		}, 5000)
+	})
 }
 
 function createNewStatus( ipAddress, res) {
-	cnx = dbConnect();
-	cnx.connect((err) => {
-		if (err) throw err;
-		post = {creatorIP:ipAddress}
-		cnx.query('INSERT INTO status SET ?', post, (err, result) => {
-			if( err) throw err
-			insertId = result.insertId
-			cnx.query('SELECT shortId FROM mapping WHERE id=?', insertId, (err, result) => {
-				cnx.end()
-				if( err) throw err;
-				shortId = result[0].shortId
-				res.status(201).json({id:shortId})
-				console.log( 'Status ' + shortId + ' created by ' + ipAddress)
-			})
+	post = {creatorIP:ipAddress}
+	cnx.query('INSERT INTO status SET ?', post, (err, result) => {
+		if( err) throw err
+		insertId = result.insertId
+		cnx.query('SELECT shortId FROM mapping WHERE id=?', insertId, (err, result) => {
+			if( err) throw err;
+			shortId = result[0].shortId
+			res.status(201).json({id:shortId})
+			console.log( 'Status ' + shortId + ' created by ' + ipAddress)
 		})
-	});
+	})
 }
 
 // generic ping
@@ -41,18 +46,14 @@ app.get("/ping", (req,res) => {
 })
 
 app.get("/:id", (req,res) => {
-	cnx = dbConnect()
-	cnx.connect( (err) => {
-		if( err) throw err;
-		cnx.query( 'SELECT mapping.shortId, status.status FROM mapping RIGHT JOIN status ON mapping.id=status.id WHERE mapping.shortId=?', req.params.id, (err, result) => {
-			if( result.length == 0){
-				res.status(404).json({id:req.params.id});
-				console.log( 'Status ' + req.params.id + ' not found')
-			} else {
-				res.status(200).json({'id':req.params.id,status:result[0].status})
-				console.log( 'Status ' + req.params.id + ' returned to ' + res.ip)
-			}
-		})
+	cnx.query( 'SELECT mapping.shortId, status.status FROM mapping RIGHT JOIN status ON mapping.id=status.id WHERE mapping.shortId=?', req.params.id, (err, result) => {
+		if( result.length == 0){
+			res.status(404).json({id:req.params.id});
+			console.log( 'Status ' + req.params.id + ' not found')
+		} else {
+			res.status(200).json({'id':req.params.id,status:result[0].status})
+			console.log( 'Status ' + req.params.id + ' returned to ' + res.ip)
+		}
 	})
 })
 
@@ -72,4 +73,6 @@ app.put("/:id/:status", (req, res) => {
 	res.status(304).json({status:"not implemented"})
 })
 
+// initiate DB connection
+dbConnect();
 app.listen(port, () => console.log('Listening on ' + port))
